0ffe1b3ba374129e019d88fae1bea0d1734dc2a3
anastasiiaboichuk <nastyaboychuc@gmail.com>
2020-12-03 19:47:14 +0200

    'add server 3'

--- server/db.js
+++ server/db.js
@@ -1,11 +1,15 @@
 const Pool = require("pg").Pool
 
+// const pool = new Pool ({
+//     user: "uurmmakgdblokc",
+//     password: "2ddd73c0d66508aba443da9cd0360938729254c0a419ec467b2aee4908cd1131",
+//     host: "ec2-52-31-233-101.eu-west-1.compute.amazonaws.com",
+//     port: 5432,
+//     database: "d9nv2noldq5r8m"
+// })
+
 const pool = new Pool ({
-    user: "uurmmakgdblokc",
-    password: "2ddd73c0d66508aba443da9cd0360938729254c0a419ec467b2aee4908cd1131",
-    host: "ec2-52-31-233-101.eu-west-1.compute.amazonaws.com",
-    port: 5432,
-    database: "d9nv2noldq5r8m"
+    connectionString: "postgres://uurmmakgdblokc:2ddd73c0d66508aba443da9cd0360938729254c0a419ec467b2aee4908cd1131@ec2-52-31-233-101.eu-west-1.compute.amazonaws.com:5432/d9nv2noldq5r"
 })
 pool.on('error', (err, client) => {
     console.error('Unexpected error on idle client', err)
